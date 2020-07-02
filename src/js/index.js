03e5e3f916c7481ba021cea466834853d4802663
ghpark <pgh7659@naver.com>
2020-07-02 15:32:11 +0900

    webpack 기본 설정: index.js -> dist/bundle.js bundling

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const currentTime = () => {
+  const date = new Date();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${hour < 10 ? '0' + hour : hour}시 ${minutes < 10 ? '0' + minutes : minutes}분 ${seconds < 10 ? '0' + seconds : seconds}초`;
+}
+
+const timerElement = document.createElement('article');
+timerElement.id = 'timer';
+
+setInterval(() => {
+  timerElement.innerHTML = currentTime();
+}, 1000);
+
+document.querySelector('#content').appendChild(timerElement);
